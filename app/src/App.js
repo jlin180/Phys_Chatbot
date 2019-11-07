11f27e49f15d7c61d1787fb4232c4fa2b9baec28
Simon <simonhe1@mail.com>
2019-11-07 12:08:52 -0500

    Made a scrolling chat with submit button

--- app/src/App.js
+++ app/src/App.js
@@ -9,12 +9,21 @@ class App extends Component {
     this.state = {
       
     };
+  
+    this.bodyStyle = {
+      width: '100vw',
+      display: 'block',
+      maxHeight: '80vh',
+      overflowY: 'auto',
+    }
+
   }
 
   render() {
     return (  
+      <div className="overflow-auto">
         <table className="table">
-          <tbody>
+          <tbody style={this.bodyStyle}>
             <tr>
               <td><User /></td>
             </tr>
@@ -24,8 +33,65 @@ class App extends Component {
             <tr>
               <td><User /></td>
             </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
+            <tr>
+              <td><User /></td>
+            </tr>
           </tbody>
         </table>
+        <div>
+          <form>
+            <div className="row">
+              <div className="col-xs-8">
+                <input 
+                  type="text"
+                  placeholder="send message"
+                />
+              </div>
+              <div className="col-xs-4">
+                <input
+                  type="submit"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
