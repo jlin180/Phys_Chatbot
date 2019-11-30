d980d082c2a706c082770c23c7a6f2a4346e5d6a
Jia Yu Lin <35271372+jlin180@users.noreply.github.com>
2019-11-30 17:22:31 -0500

    add more topics and definitions

--- server/topics.py
+++ server/topics.py
@@ -5,10 +5,16 @@
 # Unit changes
 # Heat conduction
 
-topics = {"Circuits":["circuit","parallel","series","ohms","resistor","volts","voltage","current","ammeter","amperes"], 
+topics = {"Circuits":["circuit","parallel","series","ohms","resistor","voltmeter","voltage","current","ammeter","amperes"], 
                 "Heatflow":["Heatflow","temperature","heat","heat capacity"],
-                "Unit changes":["convert units","units"],
-                "Heat conduction":["heat conduction","temperature difference","length","material","cross sectional","thermal"],
-                "Waves":["waves","speed of light","frequency","period","wavelength"],
-                "Collisions":["elastic collision","inelastic collision"]
+                "Unit changes":["convert units","units", "SI"],
+                "Heat conduction":["heat conduction","thermodynamics law","heat convention"],
+                "Waves":["waves","speed of light","frequency","period","wavelength","amplitude","wave properties","constructive interference","destructive interference","transverse wave","longitudinal wave", "polarization","diffraction","reflection","refraction"],
+                "Collisions":["elastic collision","inelastic collision"],
+                "Networks":["networks", "networks structure", "connections", "Erdos number", "nodes"],
+                "Fractal" : ["fractals", "fractal properties", "fractal dimension"],
+                "Chaos" : ["Sensitivity to Initial Condition", "Phase Space", "Invariants" ,"Strange Attractors"],
+                "Electricity and Magnetism" : ["Gauss law for electric field", "Guass law for magnetic field", "Ampere's Law", "Faraday's Law"],
+                "Quantum Mechanics" : ["Qubit","Heisenberg Uncertainty Principle","Copenhagen Interpretation","Entanglement"], 
+                "Cryptography" : ["encryption","decryption","cryptanalysis","Private Key", "Public Key"]
                 }
